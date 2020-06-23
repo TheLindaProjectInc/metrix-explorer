3a038ed973c22e81d4e2f14c030f8d284a9a6acb
nibbles83 <hardke01@yahoo.co.uk>
2020-06-23 12:32:52 +0100

    update address balance pages

--- metrix-explorer-ui/pages/address/_id/balance.vue
+++ metrix-explorer-ui/pages/address/_id/balance.vue
@@ -9,24 +9,61 @@
         <div class="dot"></div>
       </div>
     </div>
-    <table v-else>
-      <thead>
-        <tr>
-          <td>Transaction id</td>
-          <td>Time</td>
-          <td>Account Balance</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="{id, blockHeight, timestamp, balance, amount} in transactions">
-          <td>
-            <nuxt-link :to="{name: 'tx-id', params: {id: id}}">{{id | format(8,13)}}</nuxt-link>
-          </td>
-          <td>{{ timestamp | timestamp }}</td>
-          <td>{{ balance | metrix(8) }} MRX</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table" v-else>
+      <div class="table-title">
+        <div class="item-id">ID</div>
+        <div class="item-time">
+          <div class="toggler" @click="timetoggle = !timetoggle">
+            <div class="time">Time</div>
+            <div class="icon">
+              <svg data-v-59a70ea1="" xmlns="http://www.w3.org/2000/svg" width="15" height="13" viewBox="0 0 20 18">
+                <path d="M12706,731.5a4.3,4.3,0,0,1-.7,2.449,5.3,5.3,0,0,1-1.9,1.79,5.411,5.411,0,0,1-2.6.658h-11.3l1.2,0.98a0.939,0.939,0,0,1,.2.536,0.972,0.972,0,0,1-.2.535l-0.4.337a0.606,0.606,0,0,1-.5.215,1.021,1.021,0,0,1-.6-0.215l-3-2.847a0.776,0.776,0,0,1,0-1.04l3-2.847a1.013,1.013,0,0,1,.6-0.214,0.608,0.608,0,0,1,.5.214l0.4,0.337a0.936,0.936,0,0,1,.2.536,0.972,0.972,0,0,1-.2.535l-1.2.979h11.3a3.017,3.017,0,0,0,2.2-.856,2.77,2.77,0,0,0,.9-2.082,4.511,4.511,0,0,0-.2-1.194,0.709,0.709,0,0,1-.1-0.444,0.6,0.6,0,0,1,.2-0.383l0.4-.367a0.776,0.776,0,0,1,.7-0.214,0.71,0.71,0,0,1,.5.4,4.347,4.347,0,0,1,.6,2.2h0Zm-17.7-1.806a0.709,0.709,0,0,1,.1.444,0.6,0.6,0,0,1-.2.382l-0.4.368a0.773,0.773,0,0,1-.7.214,0.71,0.71,0,0,1-.5-0.4,4.347,4.347,0,0,1-.6-2.2,4.3,4.3,0,0,1,.7-2.449,5.289,5.289,0,0,1,1.9-1.791,5.4,5.4,0,0,1,2.6-.658h11.3l-1.2-.98a0.972,0.972,0,0,1-.2-0.535,0.939,0.939,0,0,1,.2-0.536l0.4-.337a0.605,0.605,0,0,1,.5-0.214,0.833,0.833,0,0,1,.6.214l3,2.847a0.777,0.777,0,0,1,0,1.041l-3,2.847a1.02,1.02,0,0,1-.6.214,0.605,0.605,0,0,1-.5-0.214l-0.4-.337a0.975,0.975,0,0,1-.2-0.535,0.939,0.939,0,0,1,.2-0.536l1.2-.98h-11.3a3.018,3.018,0,0,0-2.2.857,2.771,2.771,0,0,0-.9,2.082,4.518,4.518,0,0,0,.2,1.194h0Zm0,0" transform="translate(-12686 -721)" class="cls-1">
+                </path>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div class="item-balance">Current Balance (MRX)</div>
+        <div class="item-amount">Balance Change (MRX)</div>
+      </div>
+      <div class="table-body">
+        <div v-for="{id, blockHeight, timestamp, balance, amount} in transactions">
+          <div class="table-body-item">
+            <div class="item-id">
+              <nuxt-link class="mrx-link break-word monospace" :to="{name: 'tx-id', params: {id: id}}">{{id | format(10,10)}}</nuxt-link>
+            </div>
+            <div class="item-time" v-if="timetoggle">
+              <FromNow :timestamp="timestamp" />
+            </div>
+            <div class="item-time" v-else>
+              {{timestamp | timestamp}}
+            </div>
+            <div class="item-balance">{{ balance | metrix(8) }}</div>
+            <div class="item-amount">{{ amount | metrix(8) }}</div>
+          </div>
+          <div class="mobile-body">
+            <div class="item">
+              <div class="title">ID</div>
+              <div class="content">
+                <nuxt-link class="mrx-link break-word monospace" :to="{name: 'tx-id', params: {id: id}}">{{id | format(10,10)}}</nuxt-link>
+              </div>
+            </div>
+            <div class="item">
+              <div class="title">Time</div>
+              <div class="content">{{ timestamp | timestamp }}</div>
+            </div>
+            <div class="item">
+              <div class="title">Current Balance (MRX)</div>
+              <div class="content">{{ balance | metrix(8) }}</div>
+            </div>
+            <div class="item">
+              <div class="title">Balance Change (MRX)</div>
+              <div class="content">{{ amount | metrix(8) }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <pagination :getLink="getLink" :currentPage="currentPage" :pages="pages" />
   </div>
 </template>
@@ -46,7 +83,8 @@ export default {
       totalCount: 0,
       transactions: [],
       loading: !1,
-      currentPage: Number(this.$route.query.page || 1)
+      currentPage: Number(this.$route.query.page || 1),
+      timetoggle: !1
     };
   },
   props: {

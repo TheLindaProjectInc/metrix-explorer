8f9037b3cad94afc89c96e4a672494c863c3e80a
nibbles83 <hardke01@yahoo.co.uk>
2023-07-10 16:04:15 +0100

    Protocol version update

--- metrix-explorer-base/p2p/commands/commands/version.js
+++ metrix-explorer-base/p2p/commands/commands/version.js
@@ -14,7 +14,7 @@ class VersionMessage extends Message {
     ...options
   }) {
     super('version', options)
-    this.version = options.protocolVersion || 70023
+    this.version = options.protocolVersion || 70026
     this.nonce = nonce
     this.services = services
     this.timestamp = timestamp
